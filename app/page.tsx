ff8cdbb337bf9b3453f6ac76e54a3e0e842c2214
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-12 02:11:02 +0530

    Refine global styles and update landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,16 @@ import Designs from "@/components/Landing/Source/Designs";
 import Skills from "@/components/Landing/Skills";
 import Experience from "@/components/Landing/Experience";
 import Footer from "@/components/Landing/Footer";
+import Certificates from "@/components/Landing/Certificates";
 export default function Home() {
   return (
     <div>
       <div className="w-full h-auto border-t bg-neutral-50 dark:bg-neutral-950 px-2 flex flex-col items-center mx-auto pt-4">
         <div className="md:container lg:w-7xl w-full h-auto border-l border-r border-neutral-300 dark:border-neutral-800 mt-4">
             <Header/>
-            <div className="mt-14">
+            <div className="mt-14 relative">
               <Introduce/>
-
+            </div>
               <div className="py-4">
                 <Reviews />
               </div>
@@ -27,12 +28,6 @@ export default function Home() {
               </div>
               <div className="relative">
                 <ProductsShowcaseBento />
-              </div>
-              <div className="mt-2 w-full h-[40px] relative border-b border-t border-neutral-300 dark:border-neutral-800">
-                <StripedPattern/>
-              </div>
-              <div className="mt-20">
-                <TechRoles />
               </div>
               <div className="mt-2 w-full h-[40px] relative border-b border-t border-neutral-300 dark:border-neutral-800">
                 <StripedPattern/>
@@ -47,7 +42,12 @@ export default function Home() {
               <div className="relative">
                 <Experience />
               </div>
-
+              <div className="mt-2 w-full h-[40px] relative border-b border-t border-neutral-300 dark:border-neutral-800">
+                <StripedPattern/>
+              </div>
+              <div className="relative">
+                <Certificates />
+              </div>
               <div className="mt-2 w-full h-[40px] relative border-b border-t border-neutral-300 dark:border-neutral-800">
                 <StripedPattern/>
               </div>
@@ -70,7 +70,6 @@ export default function Home() {
                 <StripedPattern/>
               </div>
             </div>
-        </div>
       </div>
         <Footer/>
     </div>
