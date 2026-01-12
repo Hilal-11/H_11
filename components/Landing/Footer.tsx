e3d24c6f5e25e70fe5d1dd0f3a448a119d034026
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-01-13 00:52:18 +0530

    Update landing footer structure and links

--- components/Landing/Footer.tsx
+++ components/Landing/Footer.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
-import { footerConfig , socialLinks } from "@/config/footerConfigData"
-import { PiTerminalFill } from "react-icons/pi";
+import { footerConfig } from "@/config/footerConfigData"
+import { SOCIAL_LINKS } from '../../config/GeneralConfigH_11';
 import Link from 'next/link';
 import { motion } from "motion/react"
 import { StripedPattern } from '../magicui/striped-pattern';
@@ -31,7 +31,7 @@ function Footer() {
 
         {/*  start footer boxes */}
 
-        <div className='pt-4 lg:pt-10 grid lg:grid-cols-5 md:grid-cols-5 gap-4 justify-between grid-cols-2'>
+        <div className='pt-4 lg:pt-10 grid lg:grid-cols-4 md:grid-cols-4 gap-4 justify-between grid-cols-2'>
           <div className='w-full'>
             <div>
               <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">{footerConfig[0].service_title}</h1>
@@ -51,6 +51,7 @@ function Footer() {
               }
             </div>
           </div>
+
           <div className='w-full'>
             <div>
               <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">{footerConfig[1].service_title}</h1>
@@ -58,7 +59,7 @@ function Footer() {
             <div className='pl-1 py-3'>
               {
                 footerConfig[1].services?.map((service , index) => (
-                  <div className="" key={index}>
+                  <div key={index}>
                     <motion.p 
                     initial={{ scale: 1 }}
                       transition={{ duration: 0.26, ease: "easeInOut" }}
@@ -70,32 +71,16 @@ function Footer() {
               }
             </div>
           </div>
-          <div className='w-full'>
+
+
+         
+           <div className='w-full'>
             <div>
               <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">{footerConfig[2].service_title}</h1>
             </div>
             <div className='pl-1 py-3'>
               {
                 footerConfig[2].services?.map((service , index) => (
-                  <div className="" key={index}>
-                    <motion.p 
-                    initial={{ scale: 1 }}
-                      transition={{ duration: 0.26, ease: "easeInOut" }}
-                      whileHover={{ scale: 1.03, }}
-                      whileTap={{ scale: 1.03 }}
-                      className='py-1 text-neutral-500 dark:text-neutral-500 text-sm lg:text-[15px] font-sans font-medium hover:text-neutral-800 dark:hover:text-neutral-300'><Link href={service.navigateTo}>{service.service}</Link></motion.p>
-                  </div>
-                ))
-              }
-            </div>
-          </div>
-          <div className='w-full'>
-            <div>
-              <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">{footerConfig[3].service_title}</h1>
-            </div>
-            <div className='pl-1 py-3'>
-              {
-                footerConfig[3].services?.map((service , index) => (
                   <div key={index}>
                     <motion.p 
                     initial={{ scale: 1 }}
@@ -108,25 +93,35 @@ function Footer() {
               }
             </div>
           </div>
+
+          
           <div className='w-full'>
             <div>
-              <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">{footerConfig[4].service_title}</h1>
+              <h1 className="font-mono text-neutral-700 dark:text-neutral-300 text-[18px] font-medium">Social Links</h1>
             </div>
             <div className='pl-1 py-3'>
               {
-                footerConfig[4].services?.map((service , index) => (
-                  <div key={index}>
-                    <motion.p 
-                    initial={{ scale: 1 }}
-                      transition={{ duration: 0.26, ease: "easeInOut" }}
-                      whileHover={{ scale: 1.03, }}
-                      whileTap={{ scale: 1.03 }}
-                      className='py-1 text-neutral-500 dark:text-neutral-500 text-sm lg:text-[15px] font-sans font-medium hover:text-neutral-800 dark:hover:text-neutral-300' ><Link href={service.navigateTo}>{service.service}</Link></motion.p>
+                SOCIAL_LINKS.map(({id , Icon , social_link , social_name }) => (
+                  <div key={id}>
+                    <Link href={social_link}>
+                      <motion.p 
+                      initial={{ scale: 1 }}
+                        transition={{ duration: 0.26, ease: "easeInOut" }}
+                        whileHover={{ scale: 1.03, }}
+                        whileTap={{ scale: 1.03 }}
+                        className='cursor-pointer flex items-center gap-2 py-1 text-neutral-500 dark:text-neutral-500 text-sm lg:text-[15px] font-sans font-medium hover:text-neutral-800 dark:hover:text-neutral-300' ><span><Icon /></span>{social_name}</motion.p>
+                      </Link>
+                      
                   </div>
+                  
                 ))
               }
             </div>
           </div>
+
+
+
+
         </div>
         <div className='border-t border-b border-neutral-300 dark:border-neutral-800 w-full h-full flex justify-center items-center mask-l-from-60% mask-r-from-60% top-90%'>
           <h1 className='font-mono text-[7rem] md:text-[10rem] whitespace-nowrap lg:text-[14rem] font-bold text-neutral-400 dark:text-neutral-700 flex'>HILAL<span className="pl-1 whitespace-nowrap  hidden md:flex lg:flex">H_11</span></h1>
