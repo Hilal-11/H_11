6e367e5e09a7f0f6edb24b88c9527383cb2f55a0
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-04-27 00:03:52 +0530

    feat(models): introduce and refine feedback schema structure

--- lib/models/feedbackSchema.ts
+++ lib/models/feedbackSchema.ts
@@ -1,10 +1,8 @@
 import { Schema , model , models } from "mongoose"
 interface IFeedback {
-    feedback_title: string,
     feedback_discription: string
 }
 const FeedbackSchema:Schema<IFeedback> = new Schema({
-    feedback_title: { type: String, required: true, trim: true},
     feedback_discription: { type: String, required: true, trim: true }
 }, { timestamps: true })
 
