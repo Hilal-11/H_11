b41a7d480c7a7597454f93e203a398363dfc3ffc
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-05-13 13:52:17 +0530

    width fix

--- components/Landing/HeroBadge.tsx
+++ components/Landing/HeroBadge.tsx
@@ -10,10 +10,10 @@ function AnnouncementBadge({
   link?: string;
 }) {
   return (
-    <div className="z-10 flex items-center justify-center px-5">
+    <div className="z-10 flex items-center justify-center lg:px-5">
       <Link href={link ?? "#"}>
         <ShimmerButton className="shadow-2xl">
-          <span className="text-center font-sans font-samibold text-sm flex items-center gap-2 whitespace-nowrap overflow-hidden lg:w-full md:w-full w-[270px]">
+          <span className="text-center font-sans font-samibold text-sm flex items-center gap-2 whitespace-nowrap overflow-hidden lg:w-full md:w-full w-[230px]">
             <span className="flex items-center justify-center bg-neutral-100 text-center font-sans text-[11px] px-2 h-[12px] py-px rounded-full text-neutral-800">
               New
             </span>
